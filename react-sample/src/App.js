3f1a8e0b179061728e9a2700fb0cc81711328690
applego <applegorillappa@gmail.com>
2019-11-29 19:53:24 +0900

    edit html a little but not change localhost:3000

--- react-sample/src/App.js
+++ react-sample/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload..
         </p>
         <a
           className="App-link"
@@ -18,7 +18,15 @@ function App() {
         >
           Learn React
         </a>
+        <p>
+          Helloooooooooooo React.
+        </p>
       </header>
+      <div>
+        <p className="hello-React-2">
+          Helloooooooooooo Reactoooooooo.
+        </p>
+      </div>
     </div>
   );
 }
